c16b105257c11a4341dc8ebb1a201a65b0ac7fe3
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2024-09-10 07:27:31 +0530

    updated teacher's name

--- src/screens/AdminPanel.js
+++ src/screens/AdminPanel.js
@@ -19,9 +19,15 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 const teachers = [
-	{ name: 'Teacher 1' },
-	{ name: 'Teacher 2' },
-	{ name: 'Teacher 3' },
+	{ name: 'Sujit Dutta' },
+	{ name: 'Tarun Kumar Biswas' },
+	{ name: 'Sonu Yadav' },
+	{ name: 'Rabi Urang' },
+	{ name: 'Vinod Kumar' },
+	{ name: 'Leena Hatkar' },
+	{ name: 'Neeta Thakre' },
+	{ name: 'Manisha' }
+
 ];
 function AdminPanel() {
 	const [dialogOpen, setDialogOpen] = useState(false);
